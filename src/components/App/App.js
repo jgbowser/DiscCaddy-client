3a14db5e1a9df9b78b505f06adbc91f1a62dafae
John Bowser <john.bow7720@gmail.com>
2020-09-03 12:24:19 -0600

    added rough landing page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+import LandingPage from '../../routes/LandingPage/LandingPage'
 import './App.css'
 
-function App() {
-  return (
-    <h1>Hello World</h1>
-  )
+class App extends React.Component {
+  render(){
+    return (
+      <Route 
+        exact 
+        path={'/'} 
+        component={LandingPage} 
+      />
+    )
+  }
 }
 
 export default App
---
+++ src/routes/LandingPage/LandingPage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default class LandingPage extends React.Component {
+  render() {
+    return (
+      <section className='LandingPage'>
+        <h2>Welcome to DiscCaddy</h2>
+        <p>DiscCaddy is your virtual companion on the course</p>
+        <p>
+          With DiscCaddy you can create scorecards, view scorecards from previous rounds,
+          and add the discs you own to your DiscCaddy bag to get recommendations about which of your discs is best suited for your current shot.
+        </p>
+        <p>Sign in or Sign up to get started!</p>
+        <Link className='LandingPage__link' to='/login'>Sign in/Sign up</Link>
+      </section>
+    )
+  }
+
+}
