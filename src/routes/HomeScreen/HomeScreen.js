47699024d6eeafd8aa2cb7bb2b4b778c597acafa
John Bowser <john.bow7720@gmail.com>
2020-09-04 20:56:33 -0600

    added HomeScreen

--- src/routes/HomeScreen/HomeScreen.js
+++ src/routes/HomeScreen/HomeScreen.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default class HomeScreen extends React.Component {
   render() {
-    return <h1>Hello World!</h1>
+    return (
+      <section className='HomeScreen'>
+        <div className='HomeScreen__scorecards'>
+          <Link className='HomeScreen__link' to='/scorecards'>Scorecards</Link>
+        </div>
+        <div className='HomeScreen__my_bag'>
+          <Link className='HomeScreen__link' to='/my-bag'>My Bag</Link>
+        </div>
+      </section>
+    )
   }
 }
