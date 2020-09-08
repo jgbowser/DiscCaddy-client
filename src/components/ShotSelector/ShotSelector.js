0738163f7c932ce2f3c07d24b9ecc51f95b2a77a
John Bowser <john.bow7720@gmail.com>
2020-09-08 17:44:14 -0600

    added onClick to shot shaape divs to set state accordingly

--- src/components/ShotSelector/ShotSelector.js
+++ src/components/ShotSelector/ShotSelector.js
@@ -74,37 +74,84 @@ export default class ShotSelector extends React.Component {
   }
 
   renderShotsOrDiscs = () => {
-
+    const { userBag = [] } = this.context
+    const { distance } = this.state
     if(this.state.showDiscs === '') {
       return (
         <div className='DiscSelector__shots_container'>
           <div className='DiscSelector__shots_top_row'>
-            <div className='DiscSelector__shot overstable'>
+            <div 
+              className='DiscSelector__shot overstable'
+              onClick={() => this.setState({ showDiscs: 'overstable'})}
+            >
               <p>Overstable</p>
             </div>
-            <div className='DiscSelector__shot understable'>
+            <div 
+              className='DiscSelector__shot understable'
+              onClick={() => this.setState({ showDiscs: 'understable'})}
+            >
               <p>Understable</p>
             </div>
           </div>
           <div className='DiscSelector__shots_bottom_row'>
-            <div className='DiscSelector__shot straight'>
+            <div 
+              className='DiscSelector__shot straight'
+              onClick={() => this.setState({ showDiscs: 'straight'})}
+            >
               <p>Straight</p>
             </div>
-            <div className='DiscSelector__shot flex'>
+            <div 
+              className='DiscSelector__shot flex'
+              onClick={() => this.setState({ showDiscs: 'flex'})}
+            >
               <p>Flex</p>
             </div>
           </div>
+        </div>
+      )
+    } else if(this.state.showDiscs === 'overstable') {
+      const filteredDiscs = this.filterOverstableDiscs(userBag, distance)
+      return filteredDiscs.map((disc, i) =>
+        <div key={i} className='DiscSelector__disc'>
+          <span>{`${disc.brand} ${disc.name}`}</span>
+          <span>{`Speed: ${disc.speed} Glide: ${disc.glide} Turn: ${disc.turn} Fade: ${disc.fade}`}</span>
+        </div>
+      )
+    } else if(this.state.showDiscs === 'understable') {
+      const filteredDiscs = this.filterUnderstableDiscs(userBag, distance)
+      return filteredDiscs.map((disc, i) =>
+        <div key={i} className='DiscSelector__disc'>
+          <span>{`${disc.brand} ${disc.name}`}</span>
+          <span>{`Speed: ${disc.speed} Glide: ${disc.glide} Turn: ${disc.turn} Fade: ${disc.fade}`}</span>
+        </div>
+      )
+    } else if(this.state.showDiscs === 'straight') {
+      const filteredDiscs = this.filterStraightDiscs(userBag, distance)
+      return filteredDiscs.map((disc, i) =>
+        <div key={i} className='DiscSelector__disc'>
+          <span>{`${disc.brand} ${disc.name}`}</span>
+          <span>{`Speed: ${disc.speed} Glide: ${disc.glide} Turn: ${disc.turn} Fade: ${disc.fade}`}</span>
+        </div>
+      )
+    } else if(this.state.showDiscs === 'flex') {
+      const filteredDiscs = this.filterFlexDiscs(userBag, distance)
+      return filteredDiscs.map((disc, i) =>
+        <div key={i} className='DiscSelector__disc'>
+          <span>{`${disc.brand} ${disc.name}`}</span>
+          <span>{`Speed: ${disc.speed} Glide: ${disc.glide} Turn: ${disc.turn} Fade: ${disc.fade}`}</span>
         </div>
       )
     }
   }
 
   render() {
-    const { userBag = [] } = this.context
+    const { userBag = [], error } = this.context
     return (
       <section className='DiscSelector'>
         <h4>Choose a shot shape</h4>
-        {this.renderShotsOrDiscs()}
+        {error
+          ? <p className='red'>{error.message}</p>
+          : this.renderShotsOrDiscs()}
       </section>
     )
   }
