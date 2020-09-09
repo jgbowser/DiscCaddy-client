dfa1926a2cf10027d0002d313cb5c226ba676470
John Bowser <john.bow7720@gmail.com>
2020-09-09 10:38:49 -0600

    added header styling and spec file

--- src/routes/Header/Header.js
+++ src/routes/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import './Header.css'
 
 export default class Header extends React.Component {
   render() {
